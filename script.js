6eea35ec11d3181b47ebda2de3803f3c2975a4db
GautamaJr <gautamajr@hotmail.com>
2024-01-29 17:16:41 -0300

    isolando js de navegação por tabs

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  //adicionando o estado de ativo ao primeiro item
-  tabContent[0].classList.add("activo");
+  if (tabMenu.length && tabContent.length) {
+    //adicionando o estado de ativo ao primeiro item
+    tabContent[0].classList.add("activo");
 
-  // adicionar ou remover a classe 'activo'
-  function activeTab(index) {
-    tabContent.forEach((section) => section.classList.remove("activo"));
-    tabContent[index].classList.add("activo");
-  }
+    // adicionar ou remover a classe 'activo'
+    function activeTab(index) {
+      tabContent.forEach((section) => section.classList.remove("activo"));
+      tabContent[index].classList.add("activo");
+    }
 
-  //ao clicar no item da lista, exibir conteudo
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", function () {
-      activeTab(index);
+    //ao clicar no item da lista, exibir conteudo
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", function () {
+        activeTab(index);
+      });
     });
-  });
+  }
 }
+initTabNav();
