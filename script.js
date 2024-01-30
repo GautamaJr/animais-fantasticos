389348561bfb4c070ee2874f14ce25f519a4d6ae
GautamaJr <gautamajr@hotmail.com>
2024-01-30 01:37:08 -0300

    efeito de scroll suave nas opções do menu

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function initTabNav() {
 }
 initTabNav();
 
+// EFEITO JS EM PERGUNTAS E RESPOSTAS NA SECTION FAQ
 function initAccordion() {
-  // EFEITO JS EM PERGUNTAS E RESPOSTAS NA SECTION FAQ
   const accordionList = document.querySelectorAll(".js-accordion dt");
   const activeClass = "ativo";
   if (accordionList.length) {
@@ -41,3 +41,31 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+// EFEITO DE SCROLL SUAVE NO MENU
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      black: "start",
+    });
+
+    // forma alternativa
+    //   const top = section.offsetTop;
+    //   window.scrollTo({
+    //     top: top,
+    //     behavior: "smooth",
+    //   });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
